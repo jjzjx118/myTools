ece74f9e28d71f6d11c76a2103a5b84e54bcb5b4
Aaron <AT02-iOSTeam-Aaron@Anjomi-AT02.local>
2017-11-17 14:32:03 +0800

    add new method

--- JXTools/Swift/SwiftCommonTool.swift
+++ JXTools/Swift/SwiftCommonTool.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+
+extension NSString {
+    
+    func callPhone(number:String){
+        
+        let telephoneNumber = "number"
+        let str = "tel:\(telephoneNumber)"
+        let application = UIApplication.shared
+        let url = URL(string: str)
+        application.open(url!, options: [:], completionHandler: {(_ success: Bool) -> Void in
+            
+            print("OpenSuccess=\(success)")
+        })
+        
+    }
+    
+}
